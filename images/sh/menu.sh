e966be7e9814fe9c6e95c1df655342df669ec47f
jovrer <jovrer@hotmail.com>
2019-12-13 16:17:31 +0800

    update

--- images/sh/menu.sh
+++ images/sh/menu.sh
@@ -5,74 +5,6 @@ if [ -z $JO_BASEROOT ];then
     exit 'sys err'
 fi
 
-function menuMod {
-    # clear
-    
-    local modsBaseDir=$JO_SCRIPT     
-    
-    cd $modsBaseDir
-    mods=(`ls -l | egrep "^d"| awk 'BEGIN{ORS=" "} {print $NF}'`)
-    modsLen=${#mods[@]}
-    
-    local menuStr="\tchoose a image to install\n\n"
-    menuStr=$menuStr"\t------------------------------\n\n"
-    
-    local modIndex=0
-    for((; modIndex<$modsLen;modIndex++))
-    do        
-        menuStr=$menuStr"\t"$[modIndex+1]". "${mods[$modIndex]}"\n"
-    done
-
-    menuStr=$menuStr"\n\t0. Exit program\n\n"
-
-    menuStr=$menuStr"\t------------------------------\n\n"
-    menuStr=$menuStr"\tEnter option: "
-
-    echo -e $menuStr    
-    read -n 2 option    
-}
-
-function menuModShow {
-    while [ 1 ]
-    do
-        menuMod
-        # echo $modsLen
-        case $option in
-            0)
-                haltApp 
-                ;;
-            [1-$modsLen])
-                echo "ddd"
-                exit
-                menuVersionShow ${mods[$[$option-1]]}
-                break           
-                ;;
-            *)  
-                echo $option
-                logWarn "Sorry, wrong selection"        
-                ;;
-        esac        
-    done
-}
-
-
-# *****************************************************************
-# **************  choose a version to install *********************
-# ********                                                *********
-# ********    1.ddddddddddddddd                           *********
-# ********    1.ddddddddddddddd                           *********
-# ********    1.ddddddddddddddd                           *********
-# ********    1.ddddddddddddddd                           *********
-# ********    1.ddddddddddddddd                           *********
-# ********    1.ddddddddddddddd                           *********
-# ********    1.ddddddddddddddd                           *********
-# ********    1.ddddddddddddddd                           *********
-# ********    1.ddddddddddddddd                           *********
-# ********    1.ddddddddddddddd                           *********
-# *****************************************************************
-# *****************************************************************
-
-
 function menuVersion {
     # clear
         
@@ -107,35 +39,99 @@ function menuVersionShow {
         menuVersion $1
         local verBaseDir="$JO_SCRIPT/$mod"
         
+        
+        if test $option -eq 0 ; then
+            menuModShow
+            break
+        else
+            if test $option -gt 0;then
+                if test $option -le $versLen;then
+                    ver=${vers[$[$option-1]]}                
 
-        case $option in
-            0)
-                menuModShow
-                break
-            ;;
-            [1-$versLen])
-                
-                ver=${vers[$[$option-1]]}                
-
-                if [ ! -f "$verBaseDir/$ver/$JO_DOCKERFILE" ];then
-                    logErr "$verBaseDir/$ver/$JO_DOCKERFILE not exists"
-                    menuModShow
-                else 
-                    . $JO_SH"/checkAndGetArchive.sh" $mod $ver
-                    echo "build start...."
-                    dockerBuild $mod "$verBaseDir/$ver/"
-                    echo "build finish"
+                    if [ ! -f "$verBaseDir/$ver/$JO_DOCKERFILE" ];then
+                        logErr "$verBaseDir/$ver/$JO_DOCKERFILE not exists"
+                        menuModShow
+                    else 
+                        . $JO_SH"/checkAndGetArchive.sh" $mod $ver
+                        echo "build start...."
+                        dockerBuild $mod "$verBaseDir/$ver/"
+                        echo "build finish"
+                    fi
+                    break            
                 fi
-                break
-            ;;
-            *)            
-                clear
-                logWarn "Sorry, wrong selection"        
-            ;;
-        esac        
+            fi
+        fi
+
+        clear
+        logWarn "Sorry, wrong selection"
+
     done
 }
 
+
+function menuMod {
+    # clear
+    
+    local modsBaseDir=$JO_SCRIPT     
+    
+    cd $modsBaseDir
+    mods=(`ls -l | egrep "^d"| awk 'BEGIN{ORS=" "} {print $NF}'`)
+    modsLen=${#mods[@]}
+    
+    local menuStr="\tchoose a image to install\n\n"
+    menuStr=$menuStr"\t------------------------------\n\n"
+    
+    local modIndex=0
+    for((; modIndex<$modsLen;modIndex++))
+    do        
+        menuStr=$menuStr"\t"$[modIndex+1]". "${mods[$modIndex]}"\n"
+    done
+
+    menuStr=$menuStr"\n\t0. Exit program\n\n"
+
+    menuStr=$menuStr"\t------------------------------\n\n"
+    menuStr=$menuStr"\tEnter option: "
+
+    echo -e $menuStr    
+    read -n 2 option    
+}
+
+function menuModShow {
+    while [ 1 ]
+    do
+        menuMod        
+        if test $option -eq 0 ; then
+            echo "halt"
+            haltApp
+        else
+            if test $option -gt "0" ;then
+                if test $option -le $modsLen ; then
+                    menuVersionShow ${mods[$[$option-1]]}
+                    break
+                fi
+            fi
+        fi
+
+        logWarn "Sorry, wrong selection"
+    done
+}
+
+
+# *****************************************************************
+# **************  choose a version to install *********************
+# ********                                                *********
+# ********    1.ddddddddddddddd                           *********
+# ********    1.ddddddddddddddd                           *********
+# ********    1.ddddddddddddddd                           *********
+# ********    1.ddddddddddddddd                           *********
+# ********    1.ddddddddddddddd                           *********
+# ********    1.ddddddddddddddd                           *********
+# ********    1.ddddddddddddddd                           *********
+# ********    1.ddddddddddddddd                           *********
+# ********    1.ddddddddddddddd                           *********
+# ********    1.ddddddddddddddd                           *********
+# *****************************************************************
+# *****************************************************************
 
 
 clear
